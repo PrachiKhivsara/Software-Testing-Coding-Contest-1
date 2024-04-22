e81994293b6b2d77751975c1fa80dbc5f43e84d9
Prachi P <prachikhivsara@gmail.com>
2024-04-22 16:32:31 +0530

    Add files via upload

---
+++ day-6 Selenium WebElements/classexercise/src/test/java/com/example/classexercise/ClassexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassexerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
